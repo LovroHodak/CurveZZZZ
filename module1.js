66f8daa8047ab38ba3c92da84008575e778f53dc
Lovro <hodak.lovro@gmail.com>
2020-10-01 17:38:51 +0200

    This might be it

--- module1.js
+++ module1.js
@@ -1,50 +1,53 @@
 // GLOBAL VARIABLES
-
+//target players
 let player1 = document.getElementById("player1")
 let player2 = document.getElementById("player2")
-
+//keys
 let type = "keydown"
 let keysDown;
-
+//snake1
 let snake1x 
 let snake1y
 let snake1width
 let snake1height
-
+//snake2
 let snake2x
 let snake2y
 let snake2width
 let snake2height
-
+//speed
 let speed
-
+//angle
 let angle
 let angle2
-
+//move angle
 let moveAngle
 let moveAngle2
-
+//array of snake path
 let pastPos
 let pastPos2
-
+//interval
 let intervalId
-
+//score
 let player1score
 let player2score
-
+//music
 let myMusic
 myMusic = new Audio("./music/dandelion.mp3")
+myMusic.volume = 0.01
 
 let myOtherMusic
 myOtherMusic = new Audio("./music/son.mp3")
+myOtherMusic.volume = 0.01
 
 let myPauseMusic
 myPauseMusic = new Audio("./music/pausesong.mp3")
-
+myPauseMusic.volume = 0.01
+//match test vs game test
 let gameIsStarting = true
 
 // FUNCTIONS
-
+//create and start game func
 function create(){
     document.getElementById('splash').innerHTML = '<canvas id="someId" width="750" height="500"></canvas>'
     canvas = document.querySelector('canvas')
@@ -56,8 +59,57 @@ function create(){
     myMusic.play()
     stopOtherAudio(myOtherMusic)
     stopPauseMusic(myPauseMusic)
+    exitWinner1()
+    exitWinner2()
+    exitWinner()
+}
+//start game setup func
+function startGame() {
+    moveAngle = 0
+    moveAngle2 = 0
+    speed = 1
+    //left
+    if (keysDown && keysDown[37]){
+        moveAngle = -1 
+    }
+    //right
+    if (keysDown && keysDown[39]){
+        moveAngle = 1 
+    }
+    //TURBO 
+    if (keysDown && keysDown[38]){
+        newPos() 
+    }
+    //TEMPORARY PAUSE 
+    if (keysDown && keysDown[40]){
+        startGame() 
+    }
+
+    //left A
+    if (keysDown && keysDown[65]){
+        moveAngle2 = -1 
+    }
+    //right D
+    if (keysDown && keysDown[68]){
+        moveAngle2 = 1 
+    }
+    //TURBO W
+    if (keysDown && keysDown[87]){
+        newPos2() 
+    }
+    //TEMPORARY PAUSE S
+    if (keysDown && keysDown[83]){
+        startGame() 
+    }
+    
+    newPos()
+    newPos2()
+    snake1()
+    snake2()
+    snakeCollision()
 }
 
+//initialize variables func
 function initializeVariables() {
     keysDown = {}
 
@@ -82,7 +134,6 @@ function initializeVariables() {
     pastPos = []
     pastPos2 = []
     intervalId = 0
-    console.log(gameIsStarting)
     if(gameIsStarting){
         player1score = 3
         player2score = 3
@@ -93,6 +144,7 @@ function initializeVariables() {
     
 }
 
+//snake func
 function snake1() {
     ctx.save() 
     ctx.translate(snake1x, snake1y)
@@ -125,15 +177,24 @@ function snake2(){
     ctx.restore()
 }
 
-function gameOver(){
-    gameIsStarting = true
-    document.getElementById('splash').innerHTML  = `<div class="cmon"><h1>Game over</h1><p>Just click the button and play again</p><button id="button2">Start Game</button></div>`
-    document.getElementById("button2").addEventListener("click", create)
-    stopAudio(myMusic)
-    stopPauseMusic(myPauseMusic)
-    myOtherMusic.play()
+//match winner func
+function winner1() {
+    document.getElementById("player1").style.fontSize = "90px";
 }
 
+function exitWinner1() {
+    document.getElementById("player1").style.fontSize = "30px";
+}
+
+function winner2() {
+    document.getElementById("player2").style.fontSize = "90px";
+}
+
+function exitWinner2() {
+    document.getElementById("player2").style.fontSize = "30px";
+}
+
+//audio func
 function stopAudio(myMusic) {
     myMusic.pause();
     myMusic.currentTime = 0;
@@ -149,25 +210,61 @@ function stopPauseMusic(myPauseMusic){
     myPauseMusic.currentTime = 0
 }
 
+//game over and match func
+function gameOver(){
+    gameIsStarting = true
+    document.getElementById('splash').innerHTML  = `
+    <div class="cmon">
+        <h1 class="over">Game over</h1>
+        <h3>Just click the button and play again</h3>
+        <br>
+        <button id="button2">Start Game</button>
+    </div>`
+    document.getElementById("button2").addEventListener("click", create)
+    stopAudio(myMusic)
+    stopPauseMusic(myPauseMusic)
+    myOtherMusic.play()
+    exitWinner1()
+    exitWinner2()
+}
+
 function match(){
+    winner1()
     player1score--
     player1.innerText = player1score
 }
 
 function match2(){
+    winner2()
     player2score--
     player2.innerText = player2score
 }
 
 function gameOverCheck(){
     if(player1score === 0){
+        winnerBlack()
         gameOver()
     }
     if(player2score === 0){
+        winnerWhite()
         gameOver()
     }
 }
 
+//winner announcement through DOM func
+function winnerBlack(){
+    document.getElementById('demo').innerHTML = `<h1>Black player will rule them all!</h1>`
+}
+
+function winnerWhite(){
+    document.getElementById('demo').innerHTML = `<h1 class="winWhite">White player will rule them all!</h1>`
+}
+
+function exitWinner(){
+    document.getElementById('demo').innerHTML = ``
+}
+
+//collisions functions
 function snakeCollision() {
     
     if(snake1x > canvas.width || snake1x < 0 || snake1y > canvas.height || snake1y < 0){
@@ -245,6 +342,7 @@ function snakeCollision() {
     })
 }
 
+//position functions
 function newPos() {
     angle += moveAngle * Math.PI / 180 //0.01744
     //sin(0)=0        sin(1)=1
@@ -259,55 +357,12 @@ function newPos2() {
     snake2y -= speed * Math.cos(angle2)
 }
 
+//keys functions
 function logKey(e) {
     e.preventDefault()
     keysDown[e.keyCode] = (e.type == "keydown")
 }
 
-function startGame() {
-    moveAngle = 0
-    moveAngle2 = 0
-    speed = 1
-    //left
-    if (keysDown && keysDown[37]){
-        moveAngle = -1 
-    }
-    //right
-    if (keysDown && keysDown[39]){
-        moveAngle = 1 
-    }
-    //TURBO 
-    if (keysDown && keysDown[38]){
-        newPos() 
-    }
-    //TEMPORARY PAUSE 
-    if (keysDown && keysDown[40]){
-        startGame() 
-    }
-
-    //left A
-    if (keysDown && keysDown[65]){
-        moveAngle2 = -1 
-    }
-    //right D
-    if (keysDown && keysDown[68]){
-        moveAngle2 = 1 
-    }
-    //TURBO W
-    if (keysDown && keysDown[87]){
-        newPos2() 
-    }
-    //TEMPORARY PAUSE S
-    if (keysDown && keysDown[83]){
-        startGame() 
-    }
-    
-    newPos()
-    newPos2()
-    snake1()
-    snake2()
-    snakeCollision()
-}
 
 // EVENT LISTENERS
 
@@ -316,3 +371,7 @@ document.getElementById("button").addEventListener("click", create)
 document.addEventListener('keydown', logKey)
 
 document.addEventListener('keyup', logKey)
+
+
+
+  
