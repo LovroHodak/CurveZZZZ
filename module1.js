934948c765eb113cc0a9d3df432978ea0c52c623
Lovro <hodak.lovro@gmail.com>
2020-10-02 12:31:33 +0200

    0,04 0,08 0,08

--- module1.js
+++ module1.js
@@ -34,15 +34,15 @@ let player2score
 //music
 let myMusic
 myMusic = new Audio("./music/dandelion.mp3")
-myMusic.volume = 0.02
+myMusic.volume = 0.04
 
 let myOtherMusic
 myOtherMusic = new Audio("./music/son.mp3")
-myOtherMusic.volume = 0.04
+myOtherMusic.volume = 0.08
 
 let myPauseMusic
 myPauseMusic = new Audio("./music/pausesong.mp3")
-myPauseMusic.volume = 0.04
+myPauseMusic.volume = 0.08
 //match test vs game test
 let gameIsStarting = true
 
